b9aca8fa5d3bfbe9269f073b82c64585753ce240
Ariel Doyle <ariel.rose.garrett@gmail.com>
2023-01-14 15:49:53 -0800

    add initial API call function to .js file and fill out import info for index.js.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap.bundle.min.js'
+import './css/styles.css';
+import ExchangeRateService from './exchangeRateService.js';
+
+// Business
+
+function getApiData(amount) {
+  ExchangeRateService.getExchangeRate(amount)
+    .then(function(exchangeAPIResponse) {
+      if (exchangeAPIResponse instanceof Error) {
+        const errorMessage = `There was an issue accessing exchange rate data for ${amount}:
+        ${exchangeAPIResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      const description = exchangeAPIResponse.response[0].
+    })
+}
 
 //UI Logic
