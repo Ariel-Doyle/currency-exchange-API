57846afe7f7a1e5d0cf93c7c26a5a929380daf9b
Ariel Doyle <ariel.rose.garrett@gmail.com>
2023-01-23 19:26:24 -0800

    adjust error handling to account for API response.

--- src/services/exchangeRateAPI.js
+++ src/services/exchangeRateAPI.js
@@ -2,9 +2,9 @@ export default class ExchangeRateService {
   static async getExchangeRate(currencyCode, dollarAmount) {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currencyCode}/${dollarAmount}`);
-      const jsonifiedResponse = await response.json();
+      const jResponse = await response.json();
       if (!response.ok) {
-        const errorMessage = `${response.result} ${response['error-type']} ${jsonifiedResponse.message}`;
+        const errorMessage = `${jResponse.result} ${jResponse['error-type']} ${jResponse.message}`;
         throw new Error(errorMessage);        
       }
       return jsonifiedResponse;
