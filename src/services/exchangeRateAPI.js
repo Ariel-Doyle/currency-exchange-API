699be309258f90cc53fb79afa831a0ea8d568568
Ariel Doyle <ariel.rose.garrett@gmail.com>
2023-01-23 19:58:20 -0800

    make one last adjustment on error handling.

--- src/services/exchangeRateAPI.js
+++ src/services/exchangeRateAPI.js
@@ -4,7 +4,7 @@ export default class ExchangeRateService {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currencyCode}/${dollarAmount}`);
       const jResponse = await response.json();
       if (!response.ok) {
-        const errorMessage = `${jResponse.result}: ${jResponse['error-type']}: ${jResponse['extra-info']}`;
+        const errorMessage = `${response.status}: ${jResponse['error-type']}`;
         throw new Error(errorMessage);        
       }
       return jResponse;
